660fd53a5c2a050d7c382b59f060acf8e14ab081
zhangjh <njhxzhangjihong@126.com>
2022-10-08 19:12:47 +0800

    后台执行

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 ps -ef | grep spring | grep -v grep | awk '{print $2}' | xargs kill
 
-mvn clean package -DskipTests && mvn spring-boot:run
+mvn clean package -DskipTests && nohup mvn spring-boot:run &
