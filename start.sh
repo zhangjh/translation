1da683feb520be01ae605ea126627da17371c30e
zhangjh <njhxzhangjihong@126.com>
2022-11-11 13:06:48 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git pull
-ps -ef | grep spring | grep -v grep | awk '{print $2}' | xargs kill
+#git pull
+#ps -ef | grep spring | grep -v grep | awk '{print $2}' | xargs kill
 
 mvn clean package -DskipTests && nohup mvn spring-boot:run &
