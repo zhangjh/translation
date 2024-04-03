a34fa2af5cd8517a02efad5f886a80755ecb32b8
zhangjh <njhxzhangjihong@126.com>
2024-04-03 14:44:33 +0800

    增加gemini翻译接口

--- src/main/java/me/zhangjh/translate/service/GeminiTranslate.java
+++ src/main/java/me/zhangjh/translate/service/GeminiTranslate.java
@@ -22,7 +22,14 @@ public class GeminiTranslate extends TranslateEngine<String> {
     public String translateText(TranslateRequest translateRequest) {
         String text = translateRequest.getText();
         if(StringUtils.isNotEmpty(text)) {
-            String prompt = "将下面的内容进行翻译，只需返回译文即可，如果是中文翻译成英文，如果是英文翻译成中文:" + text;
+            String prompt =
+                    "将包裹在{{{}}}内的内容进行翻译，只需返回译文即可，如果是中文翻译成英文，如果是英文翻译成中文，" +
+                            "我会通过语言标识告知你待翻译内容是什么语种，zh表示中文，en" +
+                            "表示英文，如果未给出请自行判断。" +
+                            "待翻译内容: {{{ " + text + " }}}";
+            if(StringUtils.isNotEmpty(translateRequest.getTo())) {
+                prompt += ", 语种：" + translateRequest.getTo();
+            }
             TextResponse response = geminiService.generateByText(prompt);
             return response.getCandidates().get(0).getContent().getParts().get(0).getText();
         }
